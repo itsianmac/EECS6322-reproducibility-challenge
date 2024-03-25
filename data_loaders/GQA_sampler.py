e5fda401e3a238a0dc0d57b2f270cbdf32115867
itsianmac <ianmicmac@hotmail.com>
2024-03-24 20:13:00 -0400

    Re-engineer the sampling API a bit to hopefully make it less hacky and more easy to use

--- data_loaders/GQA_sampler.py
+++ data_loaders/GQA_sampler.py
@@ -1,7 +1,6 @@
 import json
 import random
-
-import pudb
+from typing import Dict, List, Tuple
 
 
 class GQA_Sampler:
@@ -41,7 +40,8 @@ class GQA_Sampler:
 
     def get_samples(self, sample_type: str = "val"):
 
-        # HACK: easier to just make this a dictionary off the top of initialization
+        # HACK: easier to just make this a dictionary off the top of initialization,
+        # or better yet, pass the number of samples etc...
         if sample_type == "val":
             group_type = self.val_group
             questions = self.val_questions
@@ -97,82 +97,44 @@ class GQA_Sampler:
 
         return sample
 
-    def initialize_visprog_sampling(self):
+    def get_visprog_gqa_samples(self, sample_sets: List) -> List[List[Dict]]:
         """
         To evaluate on a diverse set of question types (~100 detailed types),
         we randomly sample up to k samples per question type from the balanced val
         (k = 5) and test-dev (k = 20) sets."
+
+
+        Parameters
+        ----------
+        sample_sets : List
+            List of strings specifying the sample sets to return. Must be one of 'val' or 'testdev'.
+
+        Returns
+        -------
+        List[List[Dict]]
+            List of samples, each containing a list of dictionaries.
+
         """
 
         print("Initializing VisProg Sampling")
-        # Randomly sample 5 questions from each group in the val set
 
-        # ===== Get val GQA samples =====
-        self.val_sample = self.get_samples("val")
-        self.tesdev_sample = self.get_samples("testdev")
-        # for key in self.val_keys:
-        # while len(self.val_sample) < self.gqa_val_samples_per_group:
-        #     # Val has no groups, so randomly samply 5 questions from the dictionary
-        #     random_key = random.choice(list(self.val_keys))
-        #     relevant_data = {}
-        #     for relevant_key in self.val_data_keys:
-        #         relevant_data[relevant_key] = self.val_questions[random_key][
-        #             relevant_key
-        #         ]
-        #
-        #     self.val_sample.append(relevant_data)
-        #
-        # # Assert that the val sample has the correct number of samples
-        # assert len(self.val_sample) == self.gqa_val_samples_per_group
-        #
-        # # ===== Get testdev GQA samples =====
-        # # Get unique groups from the test set
-        # unique_testdev_groups = set(
-        #     data["groups"][self.testdev_group]
-        #     for data in self.testdev_questions.values()
-        # )
-        #
-        # questions_grouped_by_group = {}
-        #
-        # # Build a dictionary of unique groups and the questions that belong to them
-        # for key in self.tesdtev_keys:
-        #
-        #     data = self.testdev_questions[key]
-        #     group = data["groups"]["global"]
-        #
-        #     question_data = {}
-        #     for relevant_key in self.testdev_data_keys:
-        #         question_data[relevant_key] = data[relevant_key]
-        #
-        #     if group not in questions_grouped_by_group:
-        #         questions_grouped_by_group[group] = [question_data]
-        #     else:
-        #         questions_grouped_by_group[group].append(question_data)
-        #
-        # self.tesdev_sample = []
-        # print("Unique Testdev Groups: ", unique_testdev_groups)
-        # for group in unique_testdev_groups:
-        #     # Randomly sample testdev_samples questions from each group
-        #     group_to_sample_from = questions_grouped_by_group[group]
-        #     group_sample = []
-        #
-        #     while len(group_sample) < self.gqa_test_samples_per_group:
-        #         random_key = random.choice(group_to_sample_from)
-        #         group_sample.append(random_key)
-        #
-        #     # Ensure that the group has the correct number of samples
-        #     assert len(group_sample) == self.gqa_test_samples_per_group
-        #
-        #     # Unroll the group sample so list is 1D when appending to the testdev sample
-        #     self.tesdev_sample.extend(group_sample)
-        #
+        # ===== Get GQA samples =====
+        samples = []
+        for sample_type in sample_sets:
+            sample = self.get_samples(sample_type)
+            samples.append(sample)
+
         print("Initializing VisProg Sampling complete!")
+
+        return samples
 
 
 if __name__ == "__main__":
     # Example usage
 
-    group_key = "global"
+    # NOTE: way more local groups than global groups... so we'll use global groups
+    #       due to chatgpt limitations... even the global groups are probably too much
+    group_key = "global"  # one of 'local', 'global',
     validation_opts = {
         "path": "../data/GQA/val_balanced_questions.json",
         "data_keys": ["imageId", "question", "answer", "fullAnswer", "groups"],
@@ -188,12 +150,14 @@ if __name__ == "__main__":
     }
 
     gqa_sampler = GQA_Sampler(validation_opts, test_opts)
-    gqa_sampler.initialize_visprog_sampling()
+
+    sample_sets = ["val", "testdev"]
+    val_samples, testdev_samples = gqa_sampler.get_visprog_gqa_samples(sample_sets)
 
     # Print out the validation samples
-    print(f"Val Sample: {gqa_sampler.val_sample}")
-    print(f"Number of Val Samples: {len(gqa_sampler.val_sample)}")
+    print(f"Val Sample: {val_samples}")
+    print(f"Number of Val Samples: {len(val_samples)}")
 
     # Print out the testdev samples
-    print(f"Testdev Sample: {gqa_sampler.tesdev_sample}")
-    print(f"Number of Testdev Samples: {len(gqa_sampler.tesdev_sample)}")
+    print(f"Testdev Sample: {testdev_samples}")
+    print(f"Number of Testdev Samples: {len(testdev_samples)}")
