d27a2b02902a4302fd9bf10d09b732d4cadd97f0
ahmadsalimi <ahsa9978@gmail.com>
2024-03-28 15:49:04 -0400

    Remove temporary conditions

--- evaluation/nlvr.py
+++ evaluation/nlvr.py
@@ -14,11 +14,7 @@ def compute_stats(results_file: str) -> List[Dict[str, Any]]:
         for pair in prompt['pairs']:
             try:
                 execution_results = [program_object['results'][pair['id']]
-                                     for program_object in prompt['programs']
-                                     if isinstance(program_object, dict)  # TODO: remove this
-                                     and pair['id'] in program_object['results']]  # TODO: remove this
-                if len(execution_results) < 5:    # TODO: this should not be necessary
-                    continue
+                                     for program_object in prompt['programs']]
                 label = pair['label']
                 outcome_counts = defaultdict(lambda: 0, Counter(result['prediction']
                                                                 if isinstance(result['prediction'], Hashable)
@@ -56,9 +52,7 @@ def compute_one_run_accuracy(results_file: str, seed: int) -> float:
             try:
                 execution_results = [program_object['results'][pair['id']]
                                      for program_object in prompt['programs']
-                                     if isinstance(program_object, dict)            # TODO: remove this
-                                     and pair['id'] in program_object['results']    # TODO: remove this
-                                     and program_object['results'][pair['id']]['prediction'] is not None]
+                                     if program_object['results'][pair['id']]['prediction'] is not None]
                 result = rng.choice(execution_results or [None])
                 if result is None:
                     continue
